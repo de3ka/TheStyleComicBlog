334e2a1c1d8643d0b912fbca12734ae9fdb4dccc
de3ka <derketo@abv.bg>
2017-10-08 01:19:15 +0300

    added some hide/show navbar links functions and search function

--- public/main.js
+++ public/main.js
@@ -2,6 +2,8 @@
 "use strict";
 import blogsController from "./app/js/controllers/blogsController.js";
 import usersController from "./app/js/controllers/usersController.js";
+import strSearcher from "./helpers/search-params-verifier.js";
+import notifier from "./helpers/notifier.js";
 
 ((function() {
     let sammyApp = Sammy("#content", function() {
@@ -27,6 +29,42 @@ import usersController from "./app/js/controllers/usersController.js";
         $(document).on("click", ".register-click", () => {
             $("#create-post").show();
             $("#login").hide();
+        });
+        $(document).on("click", ".home-search-button", () => {
+            let searchVal = $("#search-input").val();
+            let newVal;
+            if (strSearcher.searchparamsCategory(searchVal)) {
+                if (searchVal === "dc") {
+                    $(location).attr("href", "#/search/category/" + strSearcher.capitalizeFirstTwo(searchVal));
+                }
+                $(location).attr("href", "#/search/category/" + strSearcher.capitalizeFirst(searchVal));
+            } else if (searchVal.includes("Comics") || searchVal.includes("comic") || searchVal.includes("comics")) {
+                $("#search-input").val("Comics");
+                newVal = $("#search-input").val();
+                $(location).attr("href", "#/search/subcategory/" + newVal);
+            } else if (searchVal.includes("Action") || searchVal.includes("action") || searchVal.includes("Figures") || searchVal.includes("figures")) {
+                $("#search-input").val("Action Figures");
+                newVal = $("#search-input").val();
+                $(location).attr("href", "#/search/subcategory/" + newVal);
+            } else if (searchVal.includes("Graphic") || searchVal.includes("graphic") || searchVal.includes("Novels") || searchVal.includes("novels") || searchVal.includes("novel")) {
+                $("#search-input").val("Graphic Novels");
+                newVal = $("#search-input").val();
+                $(location).attr("href", "#/search/subcategory/" + newVal);
+            } else {
+                notifier.send("Invalid search request");
+                notifier.explain(
+                    "Search params must be valid category/subcategory", "warning");
+            }
+            $("#search-input").val("");
+
+        });
+        $("#logout").on("click", function() {
+            localStorage.clear();
+            $("#register").show();
+            $("#login").show();
+            $("#logout").hide();
+            $("#create-post").hide();
+            notifier.send("You logged out");
         });
     });
 })());
